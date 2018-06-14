ae65dc9e0b549d240d8f58e6dcb7bdf644c19d5f
harrisonbrock <harrison@harrisonbrock.com>
2018-06-14 18:13:41 -0400

    Added flags for interrupt

--- project/ls8/cpu.js
+++ project/ls8/cpu.js
@@ -2,7 +2,7 @@
  * LS-8 v2.0 emulator skeleton code
  */
 
-
+const fs = require('fs');
 // const instruction = {
 //     HLT: 1,
 //     MUL: 170,
@@ -25,6 +25,10 @@ const FLAG_LT = 2;
 
 const IM = 0x05; // interrupt mask
 const IS = 0x06; // interrupt status R6
+
+const interruptMask = [
+    (0x1 << 0)
+];
 /**
  * Class for simulating a simple Computer (CPU & memory)
  */
@@ -43,6 +47,7 @@ class CPU {
         this.reg[IS] = 0;
         // Special-purpose registers
         this.PC = 0; // Program Counter
+        this.FL = 0;
         this.SP = this.reg[7];
 
 
@@ -64,10 +69,32 @@ class CPU {
             0b001001000: this.CALL.bind(this),
             0b001010000: this.JUMP.bind(this),
             0b00001001: this.RETURN.bind(this),
-            0b10011010: this.ST.bind(this)
+            0b10011010: this.ST.bind(this),
+            0b01000010: this.PRA.bind(this),
+            0b00001011: this.IRET.bind(this)
 
 
         };
+    }
+
+    setFlag(flag, value) {
+        value += value;
+
+        if (value) {
+            this.FL |= (1 << flag);
+        }
+        else {
+            this.FL &= ~(1 << flag);
+        }
+
+    }
+
+    getFlag(flag) {
+        return (this.FL & (1 << flag)) >> flag;
+    }
+
+    raiseInterrupt(number) {
+        this.reg[IS] |= interruptMask[number];
     }
 
     /**
@@ -84,6 +111,12 @@ class CPU {
         this.clock = setInterval(() => {
             this.tick();
         }, 1); // 1 ms delay == 1 KHz clock == 0.000001 GHz
+
+        // interrupt clock
+        this.timeInterrupt = setInterval(() => {
+
+            this.raiseInterrupt(0);
+        }, 1000);
     }
 
     /**
@@ -91,6 +124,7 @@ class CPU {
      */
     stopClock() {
         clearInterval(this.clock);
+        clearInterval(this.timeInterrupt);
     }
 
     /**
@@ -118,6 +152,31 @@ class CPU {
      * Advances the CPU one cycle
      */
     tick() {
+
+        if (this.interruptsEnable) {
+
+            const interrputs = this.reg[IS] & this.reg[IM];
+
+            for (let index = 0; index < 8; index++) {
+
+                if(((interrputs >> index) & 0x01) === 1){
+                    this.interruptsEnable = false;
+                    this.reg[IS] &= interruptMask[index];
+
+                    this._push(this.PC);
+
+                    this._push(this.FL);
+
+                    for (let register = 0; register <=6; register++) {
+                        this._push(this.reg[register]);
+                    }
+
+                    const vector = this.ram.read(0xF8 + index);
+                    this.PC = vector;
+                    break;
+                }
+            }
+        }
 
 
         const IR = this.ram.read(this.PC);
@@ -183,6 +242,18 @@ class CPU {
         this.pcAdvance = false;
     }
 
+    IRET() {
+        // pop regs off stack
+        for (let register = 6; register >=0; register--) {
+            this.reg[register] = this._pop();
+        }
+
+        this.FL = this._pop();
+        this.PC = this._pop();
+        this.pcAdvance = false;
+        this.interruptsEnable = true;
+    }
+
     ST(regA, regB) {
         this.ram.write(this.reg[regA], this.reg[regB]);
     }
@@ -190,6 +261,10 @@ class CPU {
     JUMP(operand) {
         this.PC = this.reg[operand];
         this.pcAdvance = false;
+    }
+
+    PRA(reg) {
+        fs.writeSync(process.stdout.fd, String.fromCharCode(this.reg[reg]));
     }
 
     _push(value) {
